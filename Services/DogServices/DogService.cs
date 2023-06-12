1c3c8c8b232a6757a5ab376f2e3cb6bccae3bdd6
Marla Laystrom <marlalaystrom@gmail.com>
2023-06-12 13:18:13 -0400

    updated IDogService to include the DogService Methods

--- Services/DogServices/DogService.cs
+++ Services/DogServices/DogService.cs
@@ -1,7 +1,49 @@
 
+using Data;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Models.Dogs;
+
 namespace Services.DogServices;
 
 public class DogService : IDogService
 {
+    private readonly ApplicationDbContext _context;
 
+    public DogService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> CreateDogAsync(DogCreate model)
+    {
+        var entity = new DogsEntity
+        {
+            OwnerID = model.OwnerID,
+            Name = model.Name,
+            Breed = model.Breed
+        };
+
+        _context.Dogs.Add(entity);
+        var numberOfChanges = await _context.SaveChangesAsync();
+
+        return numberOfChanges == 1;
+    }
+
+    public async Task<IEnumerable<DogDetail>> GetAllDogsAsync(int id)
+    {
+        var dogs = await _context.Dogs
+        .Where(entity => entity.OwnerID == id)
+        .Select(entity => new DogDetail
+        {
+            OwnerID = entity.OwnerID,
+            Name = entity.Name,
+
+
+        })
+         .ToListAsync();
+
+        return dogs;
+
+    }
 }
--- Services/DogServices/IDogService.cs
+++ Services/DogServices/IDogService.cs
@@ -1,8 +1,12 @@
 
 
+using Models.Dogs;
+
 namespace Services.DogServices;
 
 public interface IDogService
 {
-    
+    Task<bool> CreateDogAsync(DogCreate model);
+
+    Task<IEnumerable<DogDetail>> GetAllDogsAsync(int id);
 }
